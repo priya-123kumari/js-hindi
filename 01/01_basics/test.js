fe1d1aaead9a7b021b00abcce79d8e44b443c8a2
priya-123kumari <prihuchaudhary1052@gmail.com>
2024-06-01 07:01:11 +0000

    container config done

--- 01/01_basics/test.js
+++ 01/01_basics/test.js
@@ -3,8 +3,14 @@ const accountId = 14453
 let accountEmail= "prihuchaudhary10523@gmail.com"
 var accountPassword="12345"
 accountCity="jaipur"
+accountId=122453
+accountEmail="priya@gmail.com
+accountPassword="12234657"
+accountCity="harayana"
+
 
 
 console.log(accountId);
 console.log(accountEmail);
+console.table([accountEmail,accountPassword,accountId,accountCity]);
 
