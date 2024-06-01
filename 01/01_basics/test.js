d23644ecf8893368efbe30df8294a138d2d3cf33
priya-123kumari <prihuchaudhary1052@gmail.com>
2024-06-01 07:04:30 +0000

    container config done

--- 01/01_basics/test.js
+++ 01/01_basics/test.js
@@ -4,7 +4,7 @@ let accountEmail= "prihuchaudhary10523@gmail.com"
 var accountPassword="12345"
 accountCity="jaipur"
 accountId=122453
-accountEmail="priya@gmail.com
+accountEmail="priya@gmail.com"
 accountPassword="12234657"
 accountCity="harayana"
 
