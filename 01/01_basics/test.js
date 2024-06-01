5db61f8c9e92491326d5f1c75270791186358db5
priya-123kumari <prihuchaudhary1052@gmail.com>
2024-06-01 06:51:59 +0000

    container config done

--- 01/01_basics/test.js
+++ 01/01_basics/test.js
@@ -2,7 +2,7 @@ console.log("Priya")
 const accountId = 14453
 let accountEmail= "prihuchaudhary10523@gmail.com"
 var accountPassword="12345"
-accountCity=jaipur
+//accountCity=jaipur
 
 
 console.log(accountId);
