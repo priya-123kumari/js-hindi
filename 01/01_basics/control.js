8cae96b163dd7a1fc29000a27d957d3d59a13795
priya-123kumari <prihuchaudhary1052@gmail.com>
2024-11-10 13:39:27 +0000

    container config done

--- 01/01_basics/control.js
+++ 01/01_basics/control.js
@@ -1,1 +1,17 @@
 console.log("priya")
+const month=3
+switch(month){
+    case 1:
+        console.log("jan")
+        break;
+        case 2:
+        console.log("feb")
+        break;
+        case 3:
+        console.log("march")
+        break;
+        default:
+            console.log(("default")
+            break;
+
+}
