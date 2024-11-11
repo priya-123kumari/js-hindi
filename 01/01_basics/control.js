c1dc1b00a29cff69d897f7e97e4b1a16bf66f652
priya-123kumari <prihuchaudhary1052@gmail.com>
2024-11-11 04:18:07 +0000

    container config done

--- 01/01_basics/control.js
+++ 01/01_basics/control.js
@@ -15,10 +15,14 @@ switch(month){
             break;
 
 }
-let val1;
+//let val1;
 //val1=5??10;
 //val1=null??10;
-val1=undefined??10;
-console.log(val1);
-const iceteaprice=100
-iceteaprice>=80 ? console.log("less than 80"): console.log("more then 80")
+//val1=undefined??10;
+//console.log(val1);
+//const iceteaprice=100
+//iceteaprice>=80 ? console.log("less than 80"): console.log("more then 80")
+for(let index=0; index<=10;index++){
+    const element=index;
+    console.log(element)
+}
