b517286631c54ca549dec0536f22751252d72898
priya-123kumari <prihuchaudhary1052@gmail.com>
2024-11-19 05:28:16 +0000

    container config done

--- 01/01_basics/loops.js
+++ 01/01_basics/loops.js
@@ -45,6 +45,13 @@ coding.forEach((item)=>{
 
 //console.log(newNums);
 
-const myNums=[1,2,3,4,5,6,7,8,9,10]
-const newNums=myNums.map( (num )=>{ num+10})
+//const myNums=[1,2,3,4,5,6,7,8,9,10]
+//const newNums=myNums.map( (num )=>{ return  num+10})
+//console.log(newNums);
+const newNums=myNumers
+.map((num)=>num*10)
+.map((num)=>num+1)
 console.log(newNums);
+
+
+
