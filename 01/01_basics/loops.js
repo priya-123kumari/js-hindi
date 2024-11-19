443557410661bb2578e54192fdba861e06497ef6
priya-123kumari <prihuchaudhary1052@gmail.com>
2024-11-19 05:03:17 +0000

    container config done

--- 01/01_basics/loops.js
+++ 01/01_basics/loops.js
@@ -37,10 +37,14 @@ const coding=["java","py"]
 coding.forEach((item)=>{
   // console.log(item);
 })
-const myNums=[1,2,3,4,5,6,7,8,9,10]
+//const myNums=[1,2,3,4,5,6,7,8,9,10]
 //const newNums=myNums.filter((num)=>num>4)
 //console.log(newNums);
-const newNums=myNums.filter((num)=>{ return num>4
+//const newNums=myNums.filter((num)=>{ return num>4
 
 })
+//console.log(newNums);
+
+const myNums=[1,2,3,4,5,6,7,8,9,10]
+const newNums=myNums.map((num )=> {return num+10})
 console.log(newNums);
