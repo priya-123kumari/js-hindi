0352145b72daa0efa175944fb93414138c30547c
priya-123kumari <prihuchaudhary1052@gmail.com>
2024-11-19 05:15:05 +0000

    conyainer config done

--- 01/01_basics/loops.js
+++ 01/01_basics/loops.js
@@ -46,5 +46,5 @@ coding.forEach((item)=>{
 //console.log(newNums);
 
 const myNums=[1,2,3,4,5,6,7,8,9,10]
-const newNums=myNums.map( (num )=> num+10)
+const newNums=myNums.map( (num )=>{ num+10})
 console.log(newNums);
